f8468c69cb74f60e9059a8408ba19fc33ffaee5f
Nikita Golov <goloniko@gmail.com>
2022-09-13 15:03:04 +0300

    Test

---
+++ README.md
@@ -0,0 +1,6 @@
+# ReactJS Webpack Template
+
+* Webpack
+* TypeScript
+* CSS Modules with LESS
+* Jest unit tests
---
+++ src/Header.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './header.css';
+
+export function Header() {
+    return <h1 className={styles.example}>Hello world!</h1>
+}
